93e4843a9c335294584d98be4b1fd97d0a9426f0
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2025-02-17 13:11:44 +0100

    Update Renamer.sh

--- Renamer.sh
+++ Renamer.sh
@@ -16,7 +16,7 @@ do
     
     FILENAME = "$ARTIST$ALBUM$TITLE.mp3"
     echo "$FILENAME" >> ./filenames.txt 
-    #cp -n "$f" "$TARGETDIR"/"$FILENAME.mp3"
+    #cp -n "$f" "$TARGETDIR"/"$FILENAME"
     unset ARTIST
     unset TITLE
     unset ALBUM
